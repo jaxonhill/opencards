f314d77f4c2ccc6c7c1f73d0c5b006ec30c6e1c8
Jaxon Hill <jaxoncharleshill@gmail.com>
2025-08-12 12:18:16 -0700

    Add `GeneratingCardsPage` component

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -53,39 +53,17 @@ function CardStack() {
     );
 }
 
-export default function TestPage() {
+export function GeneratingCardsPage() {
     return (
-        <div className="flex flex-col h-full w-full justify-center items-center">
-            <div className="flex flex-col gap-3 w-[640px] pb-32">
+        <div className="flex h-full w-full items-center justify-center">
+            <section className="flex w-[640px] flex-col gap-3 pb-32">
                 <h1 className="text-neutral-100 font-medium">Generating cards</h1>
-                <div className="-space-y-61">
-                    {CARD_Z_INDEXES.map((zIndex, arrIndex) => {
-                        return (
-                            <div
-                                key={zIndex}
-                                className="w-full rounded-lg bg-neutral-900 border py-8 px-6 border-neutral-800 h-64 flex flex-col gap-2.5 justify-center relative shadow-lg"
-                                style={{
-                                    zIndex: zIndex,
-                                }}
-                            >
-                                {arrIndex === 0 && (
-                                    <>
-                                        {SKELETON_LINE_WIDTH_PERCENTAGES.map((lengthPercentage) => {
-                                            return (
-                                                <div
-                                                    key={lengthPercentage}
-                                                    className="h-6 rounded-lg bg-neutral-800"
-                                                    style={{ width: `${lengthPercentage}%` }}
-                                                ></div>
-                                            );
-                                        })}
-                                    </>
-                                )}
-                            </div>
-                        );
-                    })}
-                </div>
-            </div>
+                <CardStack />
+            </section>
         </div>
     );
+}
+
+export default function TestPage() {
+    return <GeneratingCardsPage />;
 }
