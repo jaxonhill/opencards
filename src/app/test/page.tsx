f957d155dd2c0cf066efdb6588633f360528aea3
Jaxon Hill <jaxoncharleshill@gmail.com>
2025-08-15 23:47:10 -0700

    Fix styling and remove unnecessary gap on `tabs` component

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,25 +1,5 @@
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/tabs";
-import { Braces, GalleryHorizontalEnd } from "lucide-react";
+import { EditCardsPage } from "@/features/edit-cards/components/_edit-cards-page";
 
 export default function TestPage() {
-    return (
-        <Tabs defaultValue="cards">
-            <TabsList>
-                <TabsTrigger value="cards">
-                    <GalleryHorizontalEnd />
-                    Cards
-                </TabsTrigger>
-                <TabsTrigger value="json">
-                    <Braces />
-                    JSON
-                </TabsTrigger>
-            </TabsList>
-            <TabsContent className="text-white" value="cards">
-                Cards tab content.
-            </TabsContent>
-            <TabsContent className="text-white" value="json">
-                JSON tab content.
-            </TabsContent>
-        </Tabs>
-    );
+    return <EditCardsPage />;
 }
