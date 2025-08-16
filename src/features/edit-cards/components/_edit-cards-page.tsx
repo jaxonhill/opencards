70f24fea9045900350d870b44ce563f56f7e386b
Jaxon Hill <jaxoncharleshill@gmail.com>
2025-08-15 23:46:48 -0700

    Fix spacing and styling on edit cards page

--- src/features/edit-cards/components/_edit-cards-page.tsx
+++ src/features/edit-cards/components/_edit-cards-page.tsx
@@ -2,11 +2,15 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/tabs";
 import { Braces, GalleryHorizontalEnd } from "lucide-react";
 import { SideBySideContainer } from "./side-by-side-container";
 import { Button } from "@/components/button";
+import { JsonTabContent } from "./json-tab-content";
+
+const codeContent: string =
+    '[\n\t{\n\t\t"question": "The question would go here",\n\t\t"answer": "The answer would then go here"\n\t},\n\t{\n\t\t"question": "Yet another question, #2 here",\n\t\t"answer": "Then the second answer here"\n\t},\n\t{\n\t\t"question": "Third question here. Only doing these questions to get to double digit line numbers",\n\t\t"answer": "Third answer, nice"\n\t}\n]';
 
 export function EditCardsPage() {
     return (
-        <div className="py-8 pl-6 pr-8 w-full">
-            <Tabs defaultValue="cards" className="flex flex-col gap-16 w-full">
+        <div className="pt-8 pb-6 pl-6 pr-8 w-full h-full ">
+            <Tabs defaultValue="cards" className="flex flex-col w-full h-full">
                 <div className="w-full flex items-center justify-between">
                     <TabsList>
                         <TabsTrigger value="cards">
@@ -23,11 +27,13 @@ export function EditCardsPage() {
                         Add to a deck
                     </Button>
                 </div>
-                <TabsContent className="text-white" value="cards">
-                    <SideBySideContainer headingText="1" />
+                <TabsContent className="" value="cards">
+                    <div className="mt-16">
+                        <SideBySideContainer headingText="1" />
+                    </div>
                 </TabsContent>
-                <TabsContent className="text-white" value="json">
-                    JSON tab content.
+                <TabsContent className="pt-8" value="json">
+                    <JsonTabContent json={codeContent} />
                 </TabsContent>
             </Tabs>
         </div>
