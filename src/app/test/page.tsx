9c0295e364854908af5da6a2af245fe7b530db3d
Jaxon Hill <jaxoncharleshill@gmail.com>
2025-08-17 00:07:57 -0700

    Add heading to modal and modal to test page for viewing

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,5 +1,11 @@
 import { EditCardsPage } from "@/features/edit-cards/components/_edit-cards-page";
+import { Modal } from "@/features/edit-cards/components/modal";
 
 export default function TestPage() {
-    return <EditCardsPage />;
+    // return <EditCardsPage />;
+    return (
+        <div className="flex h-full justify-center items-center">
+            <Modal />
+        </div>
+    );
 }
--- src/features/edit-cards/components/modal.tsx
+++ src/features/edit-cards/components/modal.tsx
@@ -1,12 +1,17 @@
 import { FolderIcon } from "lucide-react";
 
 export function Modal() {
-    return <div className=""></div>;
+    return (
+        <div className="bg-neutral-900 border border-neutral-800 rounded-lg py-16 px-8 w-[28rem]">
+            <h1 className="text-neutral-50 font-medium text-xl">Add cards to a deck</h1>
+            <Folder />
+        </div>
+    );
 }
 
 export function Folder() {
     return (
-        <button className="inline-flex items-center gap-1.5 py-0.5">
+        <button className="inline-flex items-center gap-1.5 py-0.5 text-neutral-400 text-sm font-normal">
             <FolderIcon className="fill-neutral-600 stroke-none size-6" />
             Programming
         </button>
