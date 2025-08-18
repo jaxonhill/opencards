4e176a4dacd7b235d7f37a2b2f19c363a4e8ec17
Jaxon Hill <jaxoncharleshill@gmail.com>
2025-08-17 19:13:39 -0700

    Add svg sizing to button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -9,7 +9,7 @@ export function Button({ variant = "default", ...props }: ButtonProps) {
         <button
             {...props}
             className={cn(
-                "w-fit font-medium text-sm rounded-lg py-2 px-3 inline-flex items-center gap-1.5 cursor-pointer disabled:cursor-not-allowed disabled:opacity-50",
+                "w-fit font-medium text-sm rounded-lg py-2 px-3 inline-flex items-center gap-1.5 cursor-pointer [&>svg]:size-4 [&>svg]:shrink-0 disabled:cursor-not-allowed disabled:opacity-50",
                 variant === "default" && "bg-neutral-100 text-neutral-900",
                 variant === "secondary" && "bg-neutral-900 text-neutral-50",
                 props.className
