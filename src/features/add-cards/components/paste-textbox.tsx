239d2a59caffe0149e8061793ce43e3a60ec643c
Jaxon Hill <jaxoncharleshill@gmail.com>
2025-08-17 20:53:10 -0700

    Add basic paste textbox component for generate ai container

---
+++ src/features/add-cards/components/paste-textbox.tsx
@@ -0,0 +1,7 @@
+export function PasteTextbox() {
+    return (
+        <div className="h-[17rem] bg-neutral-800/50 rounded-lg border border-neutral-800 text-neutral-500 text-base font-medium flex justify-center items-center w-full">
+            Paste text here...
+        </div>
+    );
+}
