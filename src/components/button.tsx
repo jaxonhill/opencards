684a9f639cacd67004f086fdf3dce08b3faa3091
Jaxon Hill <jaxoncharleshill@gmail.com>
2025-08-17 19:12:06 -0700

    Add variants to button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,14 +1,17 @@
 import { cn } from "@/lib/utils";
 
-type ButtonProps = React.ComponentProps<"button">;
+type ButtonProps = {
+    variant: "default" | "secondary";
+} & React.ComponentProps<"button">;
 
-export function Button({ ...props }: ButtonProps) {
+export function Button({ variant = "default", ...props }: ButtonProps) {
     return (
         <button
             {...props}
             className={cn(
-                "w-fit bg-neutral-100 text-neutral-900 font-medium text-sm rounded-lg py-2 px-3 inline-flex items-center gap-1.5 cursor-pointer",
-                "disabled:bg-neutral-900 disabled:text-neutral-800 disabled:cursor-not-allowed",
+                "w-fit font-medium text-sm rounded-lg py-2 px-3 inline-flex items-center gap-1.5 cursor-pointer disabled:cursor-not-allowed disabled:opacity-50",
+                variant === "default" && "bg-neutral-100 text-neutral-900",
+                variant === "secondary" && "bg-neutral-900 text-neutral-50",
                 props.className
             )}
         >
