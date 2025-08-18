093555911b7c00fb946a1615ba312e9e5f74df71
Jaxon Hill <jaxoncharleshill@gmail.com>
2025-08-17 19:19:54 -0700

    Add hover states to buttons

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -9,9 +9,9 @@ export function Button({ variant = "default", ...props }: ButtonProps) {
         <button
             {...props}
             className={cn(
-                "w-fit font-medium text-sm rounded-lg py-2 px-3 inline-flex items-center gap-1.5 cursor-pointer [&>svg]:size-4 [&>svg]:shrink-0 disabled:cursor-not-allowed disabled:opacity-50",
-                variant === "default" && "bg-neutral-100 text-neutral-900",
-                variant === "secondary" && "bg-neutral-900 text-neutral-50",
+                "w-fit font-medium text-sm rounded-lg py-2 px-3 inline-flex items-center gap-1.5 cursor-pointer transition-colors [&>svg]:size-4 [&>svg]:shrink-0 disabled:cursor-not-allowed disabled:opacity-50",
+                variant === "default" && "bg-neutral-100 text-neutral-900 hover:bg-neutral-200",
+                variant === "secondary" && "bg-neutral-900 text-neutral-50 hover:bg-neutral-800/60",
                 props.className
             )}
         >
