ce1adb15224f3dabc46b8879247387b11bb39ea7
Jaxon Hill <jaxoncharleshill@gmail.com>
2025-08-17 19:28:27 -0700

    Fix button secondary color

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -11,7 +11,7 @@ export function Button({ variant = "default", ...props }: ButtonProps) {
             className={cn(
                 "w-fit font-medium text-sm rounded-lg py-2 px-3 inline-flex items-center gap-1.5 cursor-pointer transition-colors [&>svg]:size-4 [&>svg]:shrink-0 disabled:cursor-not-allowed disabled:opacity-50",
                 variant === "default" && "bg-neutral-100 text-neutral-900 hover:bg-neutral-200",
-                variant === "secondary" && "bg-neutral-900 text-neutral-50 hover:bg-neutral-800/60",
+                variant === "secondary" && "bg-neutral-800 text-neutral-50 hover:bg-neutral-700/60",
                 props.className
             )}
         >
