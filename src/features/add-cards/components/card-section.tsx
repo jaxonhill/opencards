bfe43abce1e291dac8064b0617b4ad3696fd0285
Jaxon Hill <jaxoncharleshill@gmail.com>
2025-08-18 19:15:44 -0700

    Add card section component for add cards page

---
+++ src/features/add-cards/components/card-section.tsx
@@ -0,0 +1,49 @@
+import { CardRenderer } from "@/features/cards/components/card-renderer";
+import { CardVariant } from "@/features/cards/types/types";
+import { Copy } from "lucide-react";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/select";
+
+interface CardSectionProps {
+    cardNumber: number;
+    variant: CardVariant;
+}
+
+export function CardSection({ cardNumber, variant }: CardSectionProps) {
+    return (
+        <section className="flex flex-col gap-3">
+            <div className="flex justify-between items-center">
+                <h2 className="text-2xl font-medium text-neutral-100">{cardNumber}</h2>
+                <div className="inline-flex gap-2 items-center">
+                    <HeaderIconButton>
+                        <Copy />
+                    </HeaderIconButton>
+                    <Select>
+                        <SelectTrigger className="w-32 h-8">
+                            <SelectValue placeholder="Card Type" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectItem value="light">Light</SelectItem>
+                            <SelectItem value="dark">Dark</SelectItem>
+                            <SelectItem value="system">System</SelectItem>
+                        </SelectContent>
+                    </Select>
+                </div>
+            </div>
+            <div className="w-full flex gap-4">
+                <CardRenderer variant={variant} mode="edit" />
+            </div>
+        </section>
+    );
+}
+
+interface HeaderIconButtonProps {
+    children: React.ReactNode;
+}
+
+function HeaderIconButton({ children }: HeaderIconButtonProps) {
+    return (
+        <button className="inline-flex justify-center items-center px-2 py-2 transition-colors rounded-full text-neutral-300 [&>svg]:size-5 [&>svg]:shrink-0 hover:bg-neutral-900 hover:text-neutral-100 hover:cursor-pointer">
+            {children}
+        </button>
+    );
+}
