337447d29253d9fb6c5e8801ecd64f569de90c36
Jaxon Hill <jaxoncharleshill@gmail.com>
2025-08-18 19:36:21 -0700

    Add real variants to card section select

--- src/features/add-cards/components/card-section.tsx
+++ src/features/add-cards/components/card-section.tsx
@@ -3,6 +3,8 @@ import { CardVariant } from "@/features/cards/types/types";
 import { Copy } from "lucide-react";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/select";
 
+const CARD_VARIANTS: CardVariant[] = ["Front & Back", "Reveal Text"];
+
 interface CardSectionProps {
     cardNumber: number;
     variant: CardVariant;
@@ -17,14 +19,18 @@ export function CardSection({ cardNumber, variant }: CardSectionProps) {
                     <HeaderIconButton>
                         <Copy />
                     </HeaderIconButton>
-                    <Select>
+                    <Select defaultValue={CARD_VARIANTS[0]}>
                         <SelectTrigger className="w-32 h-8">
                             <SelectValue placeholder="Card Type" />
                         </SelectTrigger>
                         <SelectContent>
-                            <SelectItem value="light">Light</SelectItem>
-                            <SelectItem value="dark">Dark</SelectItem>
-                            <SelectItem value="system">System</SelectItem>
+                            {CARD_VARIANTS.map((variant) => {
+                                return (
+                                    <SelectItem key={variant} value={variant}>
+                                        {variant}
+                                    </SelectItem>
+                                );
+                            })}
                         </SelectContent>
                     </Select>
                 </div>
@@ -42,7 +48,7 @@ interface HeaderIconButtonProps {
 
 function HeaderIconButton({ children }: HeaderIconButtonProps) {
     return (
-        <button className="inline-flex justify-center items-center px-2 py-2 transition-colors rounded-full text-neutral-300 [&>svg]:size-5 [&>svg]:shrink-0 hover:bg-neutral-900 hover:text-neutral-100 hover:cursor-pointer">
+        <button className="inline-flex justify-center items-center px-2 py-2 transition-colors rounded-full text-neutral-400 [&>svg]:size-5 [&>svg]:shrink-0 hover:bg-neutral-900 hover:text-neutral-100 hover:cursor-pointer">
             {children}
         </button>
     );
