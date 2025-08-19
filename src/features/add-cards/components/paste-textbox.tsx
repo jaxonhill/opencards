5e59f79ebf0bbb20b61b95d543d270769a867f3f
Jaxon Hill <jaxoncharleshill@gmail.com>
2025-08-18 20:11:44 -0700

    Fix styling for generate with AI component to use smaller text

--- src/features/add-cards/components/paste-textbox.tsx
+++ src/features/add-cards/components/paste-textbox.tsx
@@ -1,6 +1,6 @@
 export function PasteTextbox() {
     return (
-        <div className="h-[17rem] bg-neutral-800/50 rounded-lg border border-neutral-800 text-neutral-500 text-base font-medium flex justify-center items-center w-full">
+        <div className="h-[17rem] bg-neutral-800/50 rounded-lg border border-neutral-800 text-neutral-500 text-sm font-medium flex justify-center items-center w-full">
             Paste text here...
         </div>
     );
