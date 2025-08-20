cfb1284fdbc15db0b1d183a8ec0b83ef50aed756
Jaxon Hill <jaxoncharleshill@gmail.com>
2025-08-20 16:40:21 -0700

    Add basic heatmap stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
-import { Sidebar } from "@/components/sidebar";
+import { DAY_GRADE, HeatmapCell } from "@/features/heatmap/components/heatmap";
+import { generateHeatmapData } from "@/features/heatmap/utils/utils";
+
+const heatmapDataRaw = generateHeatmapData();
+const heatmapData = heatmapDataRaw.splice(5);
 
 export default function Home() {
-    return <div></div>;
+    console.log(heatmapData);
+    return (
+        <div className="pl-[19rem]">
+            {heatmapData.map((cell) => {
+                return <HeatmapCell key={cell.date} dayGrade={cell.dayGrade} />;
+            })}
+        </div>
+    );
 }
