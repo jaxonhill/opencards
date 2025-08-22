e654ba2970a16705ec182203fd9c8a06e585c03b
Jaxon Hill <jaxoncharleshill@gmail.com>
2025-08-22 16:44:34 -0700

    Add component for the heatmap's month label

---
+++ src/features/heatmap-card/components/month-label.tsx
@@ -0,0 +1,19 @@
+interface HeatmapMonthLabel {
+    label: string;
+    startCol: number;
+    endCol: number;
+}
+
+export function HeatmapMonthLabel({ label, startCol, endCol }: HeatmapMonthLabel) {
+    return (
+        <div
+            className={`text-neutral-700 text-xs font-normal row-span-1`}
+            style={{
+                gridColumnStart: startCol,
+                gridColumnEnd: endCol,
+            }}
+        >
+            {label}
+        </div>
+    );
+}
