d592b9902c6af43197c073555fa1163a5066da1e
Jaxon Hill <jaxoncharleshill@gmail.com>
2025-08-22 17:00:12 -0700

    Rename heatmap component folder again and move heatmap card to a dashboard feature file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,23 @@
-import { HeatMapDisplayData } from "@/features/heatmap/types/types";
+import { HeatmapCard } from "@/features/home-dashboard/components/heatmap-card";
+import { HeatmapDisplayData } from "@/features/heatmap/types/types";
 import { generateHeatmapData } from "@/features/heatmap/utils/_temp-gen-heatmap-data";
 import { transformIntoDisplayData } from "@/features/heatmap/utils/transform-into-display-data";
+import { StartReviewCard } from "@/features/home-dashboard/components/start-review-card";
+import { DecksCard } from "@/features/home-dashboard/components/decks-card";
+import { AchievementsCard } from "@/features/home-dashboard/components/achievements-card";
 
 export default function Home() {
     const heatmapDataRaw = generateHeatmapData();
-    const displayData: HeatMapDisplayData = transformIntoDisplayData(heatmapDataRaw);
-    console.log(displayData);
+    const heatmapDisplayData: HeatmapDisplayData = transformIntoDisplayData(heatmapDataRaw);
 
-    return <div className="pl-[19rem]"></div>;
+    return (
+        <div className="pl-[19rem]">
+            <main className="grid gap-3 grid-flow-col grid-cols-12">
+                <StartReviewCard />
+                <HeatmapCard data={heatmapDisplayData} />
+                <DecksCard />
+                <AchievementsCard />
+            </main>
+        </div>
+    );
 }
--- src/features/home-dashboard/components/heatmap-card.tsx
+++ src/features/home-dashboard/components/heatmap-card.tsx
@@ -1,5 +1,6 @@
-import { HeatmapDisplayData } from "../types/types";
-import { Heatmap } from "./heatmap";
+import { HeatmapDisplayData } from "@/features/heatmap/types/types";
+import { Heatmap } from "@/features/heatmap/components/heatmap";
+import { DashboardCardWrapper } from "./dashboard-card-wrapper";
 
 interface HeatmapCardProps {
     data: HeatmapDisplayData;
@@ -7,13 +8,13 @@ interface HeatmapCardProps {
 
 export function HeatmapCard({ data }: HeatmapCardProps) {
     return (
-        <section className="flex flex-col gap-6 rounded-lg bg-neutral-900 border border-neutral-800 p-8 overflow-y-hidden w-2xl">
+        <DashboardCardWrapper className="flex flex-col gap-6">
             <h2 className="text-neutral-400 text-lg">
                 <strong className="text-neutral-100 font-medium">1,207</strong> cards studied this year
             </h2>
             <div className="w-full overflow-x-auto">
                 <Heatmap data={data} />
             </div>
-        </section>
+        </DashboardCardWrapper>
     );
 }
