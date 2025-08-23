9760153ac973246bd1b41e1be0fc9290c64d67e3
Jaxon Hill <jaxoncharleshill@gmail.com>
2025-08-22 19:25:12 -0700

    Change button to allow it to be any element so we can make it an <a> tag

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,21 +1,34 @@
 import { cn } from "@/lib/utils";
+import React from "react";
 
-type ButtonProps = {
+type PolymorphicProps<E extends React.ElementType, P> = P & {
+    as?: E;
+} & Omit<React.ComponentProps<E>, keyof P | "as">;
+
+type BaseProps = {
     variant?: "default" | "secondary";
-} & React.ComponentProps<"button">;
+    className?: string;
+    children: React.ReactNode;
+};
 
-export function Button({ variant = "default", ...props }: ButtonProps) {
+export function Button<E extends React.ElementType = "button">({
+    as,
+    variant = "default",
+    className,
+    ...props
+}: PolymorphicProps<E, BaseProps>) {
+    const Component = as || "button";
+
+    const mergedClassName = cn(
+        "w-fit font-medium text-sm rounded-lg py-2 px-3 inline-flex items-center gap-1.5 cursor-pointer transition-colors [&>svg]:size-4 [&>svg]:shrink-0 disabled:cursor-not-allowed disabled:opacity-50",
+        variant === "default" && "bg-neutral-100 text-neutral-900 hover:bg-neutral-200",
+        variant === "secondary" && "bg-neutral-800 text-neutral-50 hover:bg-neutral-700/60",
+        className
+    );
+
     return (
-        <button
-            {...props}
-            className={cn(
-                "w-fit font-medium text-sm rounded-lg py-2 px-3 inline-flex items-center gap-1.5 cursor-pointer transition-colors [&>svg]:size-4 [&>svg]:shrink-0 disabled:cursor-not-allowed disabled:opacity-50",
-                variant === "default" && "bg-neutral-100 text-neutral-900 hover:bg-neutral-200",
-                variant === "secondary" && "bg-neutral-800 text-neutral-50 hover:bg-neutral-700/60",
-                props.className
-            )}
-        >
+        <Component {...props} className={mergedClassName}>
             {props.children}
-        </button>
+        </Component>
     );
 }
